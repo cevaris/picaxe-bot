e7c370adddc4df3f450eaef4a83b45df7ca65380
Cevaris <cevaris@gmail.com>
2014-04-17 12:50:24 -0600

    can print the script out now; created a square script

--- web/controller/app/tasks.py
+++ web/controller/app/tasks.py
@@ -10,10 +10,8 @@ from app import compiler_path
 from app import usb_port
 
 @celery.task
-def move(feet,bot_script):
-
-  print "Moving forward %s spaces" % feet
-  time.sleep(10)
+def deploy(bot_script):
+  # time.sleep(5)
   
   temp_file = tempfile.NamedTemporaryFile()
   with temp_file as writer:
--- web/controller/app/views.py
+++ web/controller/app/views.py
@@ -1,16 +1,26 @@
 from flask import render_template
 
 from app import app
-from tasks import move
+from tasks import deploy
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return "Bot Controller"
 
-@app.route('/move/forward/<feet>')
+@app.route('/robot/go/<feet>')
 def move_forward(feet):
-  direction = 'forward'
-  bot_script = render_template(('%s.html' % direction), time=(int(int(feet)*2.150)))
-  move.delay(feet, bot_script)
-  return "Moving forward %s" % feet
+  bot_script = render_template('forward.html', time=(int(int(feet)*2.150)))
+  deploy.delay(bot_script)
+  return render_template('bot_script.html', direction='forward', feet=feet, bot_script=bot_script)
+  # return "Moving forward %s \n" % (feet, bot_script)
+
+
+@app.route('/robot/square/<feet>')
+def move_forward(feet):
+  bot_script = render_template('square.html', time=(int(int(feet)*2.150)))
+  deploy.delay(bot_script)
+  return render_template('bot_script.html', direction='in a Square of', feet=feet, bot_script=bot_script)
+
+
+
